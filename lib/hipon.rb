050ee92cd93286fb758e00b886a015dd8c4a9a59
ajcubeta <ajcubeta@gmail.com>
2013-04-05 17:16:32 +0800

    Generate initial documents to booze-up

---
+++ lib/hipon.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+# Copyright (c) 2013 ajcubeta
+#
+require 'rubygems'
+
+gem "activemodel", "~>3.2.13"
+gem "will_paginate", "~>3.0.3"
+
+require "singleton"
+require 'will_paginate/collection'
---
+++ lib/hipon/version.rb
@@ -0,0 +1,4 @@
+# encoding: utf-8
+module Hipon
+  VERSION = "0.1"
+end
